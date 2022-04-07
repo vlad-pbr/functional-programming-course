5291d99749ebc8eb5cd602941780faee86383c6e
Vlad Poberezhny <vlad.pbr@gmail.com>
2022-04-08 01:20:57 +0300

    feat(util): implemented map

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,9 @@
   println("Hello world!")
 
   val nums: List[Int] = List(1, 2, 3, 4)
+
   println(Util.max(nums, (x: Int, y: Int) => x - y))
+  Util.map(nums,(x:Int)=>x*2,(y:Int)=>"student "+y).foreach(s=>println(s))
 
 object Util {
 
@@ -10,5 +12,9 @@ object Util {
   def max[A](list: List[A], comparator: (x: A, y: A) => Int): A = list.length match
     case 1 => list(0)
     case default => if comparator(list(0),list(1)) > 0 then max(list.filter(_ != list(1)), comparator) else max(list.filter(_ != list(0)), comparator)
+
+  // given a list of As, translate each A to B and each B to C
+  def map[A,B,C](list: List[A], _AtoB: (a: A) => B, _BtoC: (b: B) => C): List[C] =
+    list.map(a => _AtoB(a)).map(b => _BtoC(b))
 
 }
