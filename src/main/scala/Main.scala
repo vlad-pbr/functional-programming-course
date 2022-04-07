871be4a61fa14e025cfd1d115ca28d343ca5f04b
Vlad Poberezhny <vlad.pbr@gmail.com>
2022-04-08 01:11:48 +0300

    feat(util): implemented max

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,14 @@
 @main def hello: Unit = 
   println("Hello world!")
-  println(msg)
 
-def msg = "I was compiled by Scala 3. :)"
+  val nums: List[Int] = List(1, 2, 3, 4)
+  println(Util.max(nums, (x: Int, y: Int) => x - y))
+
+object Util {
+
+  // given a list of As and a comparator (A, A) -> Int, get biggest A
+  def max[A](list: List[A], comparator: (x: A, y: A) => Int): A = list.length match
+    case 1 => list(0)
+    case default => if comparator(list(0),list(1)) > 0 then max(list.filter(_ != list(1)), comparator) else max(list.filter(_ != list(0)), comparator)
+
+}
