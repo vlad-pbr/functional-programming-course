c8e346a6213ab7609e1918d71b4c8226e02bb250
Vlad Poberezhny <vlad.pbr@gmail.com>
2022-04-08 20:05:19 +0300

    fix(util): various fixes for compatibility with scala 2 (-_-)

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -1,4 +1,3 @@
-import scala.collection.MapView
 import scala.math.log10
 import scala.math.pow
 import scala.math.sqrt
@@ -6,31 +5,29 @@ import scala.math.sqrt
 object Util {
 
   // given a list of As and a comparator (A, A) -> Int, get biggest A
-  def max[A](list: List[A], comparator: (x: A, y: A) => Int): A = list.length match
+  def max[A](list: List[A], comparator: (A, A) => Int): A = list.length match {
     case 1 => list(0)
-    case default => if comparator(list(0),list(1)) > 0 then max(list.filter(_ != list(1)), comparator) else max(list.filter(_ != list(0)), comparator)
+    case default => if (comparator(list(0),list(1)) > 0) max(list.filter(_ != list(1)), comparator) else max(list.filter(_ != list(0)), comparator)
+  }
 
   // given a list of As, translate each A to B and each B to C
-  def map[A,B,C](list: List[A], _AtoB: (a: A) => B, _BtoC: (b: B) => C): List[C] =
+  def map[A,B,C](list: List[A], _AtoB: (A) => B, _BtoC: (B) => C): List[C] =
     list.map(a => _AtoB(a)).map(b => _BtoC(b))
 
   // given an list of As and a comparator, return true if list is sorted
-  def isSorted[A](list: List[A], comparator: (x: A, y: A) => Boolean): Boolean = list.length match
+  def isSorted[A](list: List[A], comparator: (A, A) => Boolean): Boolean = list.length match {
     case 1 => true
-    case default => if comparator(list(0),list(1)) then isSorted(list.filter(_ != list(0)), comparator) else false
+    case default => if (comparator(list(0),list(1))) isSorted(list.filter(_ != list(0)), comparator) else false
+  }
 
   // given an array of doubles, find probability for each i in array to appear
   def probs(array: Array[Double]): Array[Double] = 
     array.map(a => array.groupBy(identity).mapValues(_.size / array.length.toDouble)(a) ) 
 
   // calculate entropy from given array
-  def entropy(array: Array[Double]): Double =
-
-    def entropyWithProbs(arr: Array[Double], probsMap: MapView[Double, Double]): Double = arr match
-      case a if a.length == 0 => 0.0
-      case default => ( probsMap(arr(0)) * (log10(probsMap(arr(0))) / log10(2.0) ) ) + entropyWithProbs(arr.filter(_ != arr(0)), probsMap)
-
-    -entropyWithProbs(array, array.groupBy(identity).mapValues(_.size / array.length.toDouble))
+  def entropy(array: Array[Double]): Double = {
+    -probs(array).map(p => p * (log10(p) / log10(2.0)) ).sum
+  }
 
   // calculate expected value from given array
   def mu(array: Array[Double]): Double = 
@@ -40,13 +37,9 @@ object Util {
   def variance(array: Array[Double]): Double = 
     array.map(a => (1 / array.length.toDouble) * pow( a - mu(array), 2 ) ).sum
 
-  // calculate standard deviation from given variance
-  def standard_deviation_from_variance(variance: Double): Double =
-    sqrt(variance)
-
   // calculate z-score from given array
   def zscore(array: Array[Double], x: Double) = 
-    (x - mu(array)) / standard_deviation_from_variance(variance(array))
+    (x - mu(array)) / sqrt(variance(array))
 
   // calculate covariance from two arrays
   def cov(arrayX: Array[Double], arrayY: Array[Double]): Double =
@@ -54,6 +47,6 @@ object Util {
 
   // calculate pearson correlation from two arrays
   def pearson(arrayX: Array[Double], arrayY: Array[Double]): Double =
-    cov(arrayX, arrayY) / ( standard_deviation_from_variance(variance(arrayX)) * standard_deviation_from_variance(variance(arrayY)) )
+    cov(arrayX, arrayY) / ( sqrt(variance(arrayX)) * sqrt(variance(arrayY)) )
 
 }
